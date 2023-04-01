5e74a55f633b402b40621e66d6e67980acef7ada
Smiley <jcsmileyjr@gmail.com>
2023-04-01 10:41:53 -0500

    Documentation: Todo list comments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import './App.css';
 
+/** TODO
+ * Setup the background color
+ * Setup general layout (input, slider, checkboxs)
+ * Setup mobile and desktop view
+ * Setup the structure elements
+ * Add general styles
+ * Test responsive design
+ * Add state and functionality for input
+ * Functionality for copy button
+ * Add state and functionality for slider
+ * Link Slider and input functionality
+ * "Include Symbol" functionality
+ * etc. for each checkbox
+ *  
+ * @returns 
+ */
 function App() {
   return (
     <div className="App">
