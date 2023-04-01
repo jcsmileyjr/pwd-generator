35d3398587303c514634df608d2c1cccc1860e20
Smiley <jcsmileyjr@gmail.com>
2023-04-01 11:37:22 -0500

    Setup input container and elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import CopyIcon from './assets/copy.png';
 
 /** TODO
  * Setup the background color - DONE
  * Setup general layout (input, slider, checkboxs) - DONE
  * Setup mobile and desktop view - DONE
- * Setup the structure elements
+ * Setup the structure elements - DONE
  * Add general styles
  * Test responsive design
  * Add state and functionality for input
@@ -18,9 +19,10 @@ import "./App.css";
  */
 function App() {
   return (
-    <main className="App">
+    <main>
       <div className="passwordInput--container">
-        Password Input Container
+        <input type="text" className="input--style" />
+        <img src={CopyIcon} className="copyIcon--style" alt="Clickable button to copy password to clipboard" />
       </div>
       <div className="passwordSlider--container">
         Password Slider Container
