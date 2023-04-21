25d4baa06f9bfeaf598af13dba11855622646073
Smiley <jcsmileyjr@gmail.com>
2023-04-21 17:03:07 -0500

    Add UI tests

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ import {useState} from "react";
  * Add general styles
  * Test responsive design
  * Add initial tests (should pass)
+ *  1. That the UI elements are displayed
  * Add functional tests (should fail)
- * Add state and functionality for input
- *  - onChange
+ *  1. Test that the output changes
+ * Add state - DONE
+ * functionality for input
  * Functionality for copy button
  * Add state and functionality for slider
  * Link Slider and input functionality
@@ -34,32 +36,32 @@ function App() {
   return (
     <main>
       <div className="passwordInput--container">
-        <input type="text" className="input--style" name="pwd-viewer" />
+        <input type="text" aria-label="view-password" className="input--style" name="pwd-viewer" />
         <img src={CopyIcon} className="copyIcon--style" alt="Clickable button to copy password to clipboard" />
       </div>
       <div className="passwordSlider--container">
-        <input type="range" min="1" max="15" value="9" className="slider" name="pwd-slider" />
+        <input type="range" aria-label="password character range" min="1" max="15" value="9" className="slider" name="pwd-slider" />
         <p className="charaterText--style">12 characters</p>
       </div>
       <div className="passwordCheckboxs--container">
         <label htmlFor="symbolCheckbox">
-          <input type="checkbox" name="symbolCheckbox" onChange={() => setSymbolCheckbox(!symbolCheckbox)} />
-          Include Symbols {symbolCheckbox}<span className="charater--style">(@#$%) </span>
+          <input type="checkbox" id="symbolCheckbox" onChange={() => setSymbolCheckbox(!symbolCheckbox)} />
+          Include Symbols <span className="charater--style">(@#$%) </span>
         </label>
         <label htmlFor="numberCheckbox">
-          <input type="checkbox" name="numberCheckbox" onChange={() => setNumberCheckbox(!numberCheckbox)} />
+          <input type="checkbox" id="numberCheckbox" onChange={() => setNumberCheckbox(!numberCheckbox)} />
           Include Numbers <span className="charater--style">(1234)</span>
         </label>
         <label htmlFor="lowerCaseCheckbox">
-          <input type="checkbox" name="lowerCaseCheckbox" onChange={() => setLowerCaseCheckbox(!lowerCaseCheckbox)} />
+          <input type="checkbox" id="lowerCaseCheckbox" onChange={() => setLowerCaseCheckbox(!lowerCaseCheckbox)} />
           Include Lowercase Characters <span className="charater--style">(abcd)</span> 
         </label>
         <label htmlFor="upperCaseCheckbox">
-          <input type="checkbox" name="upperCaseCheckbox" onChange={() => setUpperCaseCheckbox(!upperCaseCheckbox)} />
+          <input type="checkbox" id="upperCaseCheckbox" onChange={() => setUpperCaseCheckbox(!upperCaseCheckbox)} />
           Include UpperCase Characters <span className="charater--style">(ABCD)</span> 
         </label>
         <label htmlFor="excludeCheckbox">
-          <input type="checkbox" name="excludeCheckbox" onChange={() => setExcludeCheckbox(!excludeCheckbox)} />
+          <input type="checkbox" id="excludeCheckbox" onChange={() => setExcludeCheckbox(!excludeCheckbox)} />
           Exclude Similar Characters <span className="charater--style">(ABCD)</span> 
         </label>
       </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("primary screen UI elements are there", () => {
+  test("that the password is displayed", () => {
+    render(<App />);
+    screen.getByLabelText('view-password')
+  })
+
+  test("that a character range control and the current number of characters are displayed", () => {
+    render(<App />);
+    screen.getByLabelText('password character range')
+    screen.getByText(/12 characters/i);
+  })
+
+  test("that all checkboxes are visible", () => {
+    render(<App />);
+    screen.getByRole("checkbox", {name: "Include Symbols (@#$%)"});
+    screen.getByRole("checkbox", {name: "Include Numbers (1234)"});
+    screen.getByRole("checkbox", {name: "Include Lowercase Characters (abcd)"});
+    screen.getByRole("checkbox", {name: "Include UpperCase Characters (ABCD)"});
+    screen.getByRole("checkbox", {name: "Exclude Similar Characters (ABCD)"});
+  });
 });
